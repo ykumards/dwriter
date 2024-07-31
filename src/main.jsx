5c86653a49f2f842d6ee4b867b37ebcc57e441ba
Yogesh Kumar <ykumar@nyu.edu>
2024-07-31 15:44:29 +0300

    rename file

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import GlobalStyles from './GlobalStyles.js';
+import GlobalStyles from './GlobalStyles';
 import { EditorProvider } from './context/EditorContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
